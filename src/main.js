1996c16caa01b87ef4a7622e2e5b6ac9426f3548
Neefko <ri79004748947@gmail.com>
2025-04-07 22:09:24 +0300

    v0.1

--- src/main.js
+++ src/main.js
@@ -1,18 +1,27 @@
 const { invoke } = window.__TAURI__.core;
+const { emit } = window.__TAURI__.event;
 
 let greetInputEl;
 let greetMsgEl;
 
 async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+}
+
+async function closeWindow() {
+  await emit("close-win");
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
+
   document.querySelector("#greet-form").addEventListener("submit", (e) => {
     e.preventDefault();
     greet();
+  });
+
+  document.querySelector("#close-btn").addEventListener("click", () => {
+    closeWindow();
   });
 });
