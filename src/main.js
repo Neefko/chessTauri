8a30ab49cc8231ca1056edbbe4e1530b541b753f
Neefko <ri79004748947@gmail.com>
2025-04-08 22:07:18 +0300

    v0.1

--- src/main.js
+++ src/main.js
@@ -1,26 +1,11 @@
 const { invoke } = window.__TAURI__.core;
 const { emit } = window.__TAURI__.event;
 
-let greetInputEl;
-let greetMsgEl;
-
-async function greet() {
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-
 async function closeWindow() {
-  await emit("close-win");
+  await invoke("close_window");
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-
   document.querySelector("#close-btn").addEventListener("click", () => {
     closeWindow();
   });
